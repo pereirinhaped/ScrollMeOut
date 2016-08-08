182f049d1a572178067bdacf6ea641070d3f191d
Pedro Pereirinha <pereirinhaped@gmail.com>
2016-08-08 16:36:11 +0100

    Swipe gesture implemented

--- ScrollMeOut/ViewController.swift
+++ ScrollMeOut/ViewController.swift
@@ -13,6 +13,11 @@ class ViewController: UIViewController {
 	@IBOutlet weak var mainScrollView: UIScrollView!
 	
 	var images = [UIImageView]()
+	var MAX_PAGE: Int {
+		return images.count - 1
+	}
+	let MIN_PAGE = 0
+	var CUR_PAGE = 0
 	
 	override func viewDidLoad() {
 		super.viewDidLoad()
@@ -21,8 +26,8 @@ class ViewController: UIViewController {
 	override func viewDidAppear(_ animated: Bool) {
 		
 		// mainScrollView.backgroundColor = UIColor.blue.withAlphaComponent(0.3)
+		
 		mainScrollView.clipsToBounds = false
-		mainScrollView.frame.size.width = 235
 		
 		for x in 0...2 {
 			let image = UIImage(named: "icon\(x).png")
@@ -43,6 +48,22 @@ class ViewController: UIViewController {
 		mainScrollView.contentSize = contenSize
 	}
 
+	@IBAction func detectSwipe(_ sender: UISwipeGestureRecognizer) {
+		
+		if CUR_PAGE < MAX_PAGE && sender.direction == .left {
+			moveScrollView(direction: 1)
+		}
+		
+		if CUR_PAGE > MIN_PAGE && sender.direction == .right {
+			moveScrollView(direction: -1)
+		}
+	}
 
+	func moveScrollView(direction: Int) {
+		CUR_PAGE = CUR_PAGE + direction
+		let newOffSet = CGPoint(x: mainScrollView.frame.width * CGFloat(CUR_PAGE), y: 0)
+		mainScrollView.setContentOffset(newOffSet, animated: true)
+	}
+	
 }
 
